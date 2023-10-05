36104e8e81faf832ba70ef4aa3b10b9eae0c5813
Justin Prasad <justineprasad003@gmail.com>
2023-10-05 12:06:49 -0400

    Added 'In progress' sign

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import  Nav from './components/nav.js'
-
+import TypeIt from 'typeit-react'
 function App() {
   return (
+    <>
     <Nav></Nav>
+    <div class="progress">
+      <TypeIt>Welcome! This site is in progress</TypeIt>
+    </div>
+    
+    </>
   );
 }
 
